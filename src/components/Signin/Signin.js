1a875040f46a3dc95efd617163a22cdc116ec879
sbatugul <sbatuhuangul@hotmail.com>
2024-01-17 19:08:23 +0300

    update

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://smartbrainapp-8q6s.onrender.com/signin', {
+        fetch('https://smartbrainbe-stdo.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
