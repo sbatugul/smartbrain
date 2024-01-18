cc88e3c8da24ea73bd48ec4f27487f20c6fc97c7
sbatugul <sbatuhuangul@hotmail.com>
2024-01-18 15:47:44 +0300

    update

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -6,76 +6,86 @@ class Signin extends React.Component {
         super(props);
         this.state = {
             signInEmail: '',
-            signInPassword: ''
+            signInPassword: '',
+            signInMessage: '' 
         }
     }
+
     onEmailChange = (event) => {
-        this.setState({signInEmail: event.target.value})
+        this.setState({ signInEmail: event.target.value })
     }
 
     onPasswordChange = (event) => {
-        this.setState({signInPassword: event.target.value})
+        this.setState({ signInPassword: event.target.value })
     }
 
     onSubmitSignIn = () => {
         fetch('https://smartbrainbe-stdo.onrender.com/signin', {
             method: 'post',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 email: this.state.signInEmail,
                 password: this.state.signInPassword
             })
         })
-    
-        .then(response => response.json())
-        .then(user => {
-          if(user.id){ 
-            this.props.loadUser(user);
-            this.props.onRouteChange('home');
-          }
-        })
+            .then(response => response.json())
+            .then(user => {
+                if (user.id) {
+                    this.props.loadUser(user);
+                    this.props.onRouteChange('home');
+                } else {
+                    this.setState({ signInMessage: 'Invalid email or password' });
+                }
+            })
+            .catch(err => {
+                console.log("Error signing in:", err);
+                this.setState({ signInMessage: 'An error occurred while signing in' });
+            });
     }
-        render() {
+
+    render() {
         const { onRouteChange } = this.props;
         return (
-        <article className=" br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className=" pa4 black-80">
-                <div className=" measure center">
-                    <fieldset id="sign_up" className=" ba b--transparent ph0 mh0">
-                    <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                    <div className=" mt3">
-                        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="email" 
-                            name="email-address"  
-                            id="email-address"
-                            onChange={this.onEmailChange}
-                        />
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw6 lh-copy f6" for="password">Password</label>
-                        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="password" 
-                            name="password"  
-                            id="password"
-                            onChange={this.onPasswordChange}
-                        />
-                    </div>
-                    </fieldset>
-                    <div className="">
-                        <input 
-                            onClick={this.onSubmitSignIn}
-                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                            type="submit" 
-                            value="Sign in"
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main className="pa4 black-80">
+                    <div className="measure center">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                            <legend className="f1 fw6 ph0 mh0">Welcome to the Smartbrain app!</legend>
+                            <p className="f4 fw6 ph0 mh0">Please sign in to try the artificial intelligence that finds the face in the picture. If you are not registered, please register.</p>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    type="email"
+                                    name="email-address"
+                                    id="email-address"
+                                    onChange={this.onEmailChange}
+                                />
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={this.onPasswordChange}
+                                />
+                            </div>
+                        </fieldset>
+                        <div className="">
+                            <input
+                                onClick={this.onSubmitSignIn}
+                                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                                type="submit"
+                                value="Sign in"
                             />
                         </div>
-                    <div className="lh-copy mt3">
-                    <p onClick={() => onRouteChange('register')}  className="f6 link dim black db pointer">Register</p>
+                        <p className="red">{this.state.signInMessage}</p> 
+                        <div className="lh-copy mt3">
+                            <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+                        </div>
                     </div>
-                </div>
-            </main>
-        </article>
+                </main>
+            </article>
         );
     }
 }
