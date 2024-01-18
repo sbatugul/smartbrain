07faa37f2b1b4f442868150a2c2fb65591e2a080
sbatugul <sbatuhuangul@hotmail.com>
2024-01-18 14:47:43 +0300

    update

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,5 +1,3 @@
-// FaceRecognition.js
-
 import React from "react";
 import "./FaceRecognition.css";
 
@@ -7,21 +5,18 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img id="inputimage" src={imageUrl} alt="" width="500px" height="auto" />
-        {boxes.map((box, index) => (
-          <div
-            key={index}
-            className="bounding-box"
-            style={{
-              top: box.topRow,
-              right: box.rightCol,
-              bottom: box.bottomRow,
-              left: box.leftCol
-            }}
-          >
-            <p>{box.confidence}</p>
-          </div>
-        ))}
+        <img id="inputimage" src={imageUrl} alt="" width="500px" height="auto" onLoad={() => {}} />
+        {Array.isArray(boxes) && boxes.length > 0 ? (
+          boxes.map((box, index) => (
+            <div
+              key={index}
+              className="bounding-box"
+              style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}
+            ></div>
+          ))
+        ) : (
+          <p>No faces detected</p>
+        )}
       </div>
     </div>
   );
