4b173a7b84dbe116ee1f128d227f6a36521735a9
sbatugul <sbatuhuangul@hotmail.com>
2024-01-18 10:56:49 +0300

    update

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./FaceRecognition.css";
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
+  // Ensure that boxes is an array before trying to map
+  if (!Array.isArray(boxes)) {
+    return null; // or handle it in some other way based on your application logic
+  }
+
   return (
     <div className="center ma">
       <div className="absolute mt2">
