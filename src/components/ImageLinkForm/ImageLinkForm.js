150719155490bd3458868864819e0aa0d9caf308
sbatugul <sbatuhuangul@hotmail.com>
2024-01-18 15:59:41 +0300

    update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className="f3 ">
-                {'Please provide an image URL(not longer than 2000 characters) to make the search... magic Brain will detect faces in your pictures. Give it a try!'}
+                {'Please provide an image URL(not longer than 2000 characters) to make the search.Give it a try!'}
             </p>
             <div className="center">
                 <div className="form center pa4 br 3 shadow-5">
